a2ae10bcdbbf14a8ae0307e45bbef7b5cfaf990d
OneJumpMan <79946649+OneJumpMan@users.noreply.github.com>
2021-03-23 17:38:44 -0400

    Add files via upload

---
+++ stage.py
@@ -0,0 +1,19 @@
+from game.sprite import Sprite
+
+class Stage(Sprite):
+    def __init__(self):
+        self.set_velocity(0, 0)
+        
+    def main_platform(self):
+        pass
+        # define shape
+
+    def right_platform(self):
+        pass
+        # define shape
+
+    def left_platform(self):
+        pass
+        # define shape
+
+    
