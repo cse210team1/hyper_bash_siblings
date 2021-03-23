f9778f0bc0312afecc65b214c2956613a6dd117f
Spencer Wadsworth <sjw1999@byui.edu>
2021-03-23 18:33:08 -0400

    lots of additions

--- game/characters/alice.py
+++ game/characters/alice.py
@@ -1,6 +1,15 @@
 from game.fighter import Fighter
+from game.characters.fighter_numbers import alice
 
 class Alice(Fighter):
     def __init__(self):
         super().__init__()
+        self.set_ground_speed(alice["ground_speed"])
+        self.set_air_speed(alice["air_speed"])
+        self.set_damage_multiplier(alice["damage_multiplier"])
+        self.set_a_attack(alice["a_attack"])
+        self.set_b_attack(alice["b_attack"])
+
+    
+
         
